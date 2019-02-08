c0e2e17639191f567644767c3be8298333abdbc3
SeanConway512 <Sean.Conway@students.ittralee.ie>
2019-02-08 10:54:33 +0000

    Add files via upload

---
+++ Interactable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    public string Name;
+    public string InteractText = "press E to interact";
+
+    public virtual void onInteract()
+    {
+
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ InteractableConsole.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableConsole : Interactable {
+    private bool IsOn = false;
+    public override void onInteract()
+    {
+        InteractText = "Press E to ";
+        IsOn = !IsOn;
+        
+    }
+}
