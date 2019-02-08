423d81fdb3e7aa0c60c61db208c4b349fd0565b4
SeanConway512 <Sean.Conway@students.ittralee.ie>
2019-02-08 10:54:02 +0000

    Add files via upload

---
+++ UnityFolder/PickUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(90 * Time.deltaTime, 0.5f, 0.5f);
+	}
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
